54a48253c85d98a53ff62137e774da3cd47f4792
aparks4 <arynmparks@gmail.com>
2022-08-29 08:05:48 -0700

    set up server.js

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+require('dotenv').config();
 
 const app = express();
 const PORT = 4000;
 
+app.set('view engine', 'ejs');
 
-
-
+// home route
 app.get('/', (req, res) => {
     res.send('hello world');
 })
 
-
+// 404 Wildcard Route
+app.route('/*').all((req, res) => {
+    res.render('404');
+})
 
 
 
